37bfaf94bce2818df786891ec7c9af873bada7a8
AarthiK03 <155801991+AarthiK03@users.noreply.github.com>
2024-05-16 11:03:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-docker-compose up -d
-docker login -u ar8888  -p dckr_pat_NqD6cUw-P2zyV9GQ715-Y8hLK_I
-
+docker login -u AarthiK03  -p $DOCKER_PASSWORD
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
-       sh'chmod +x build.sh'
-       sh'./build.sh'
+       sh 'chmod +x build.sh'
+       sh './build.sh'
 
-        docker tag test ar8888/dev
-        docker push vennilavan/dev
+        docker tag test AarthiK/dev
+        docker push AarthiK/dev
 
-if [[ $GIT_BRANCH == "origin/master" ]]; then
-        sh'chmod +x build.sh'
-        sh'./build.sh'
+if [[ $GIT_BRANCH == "origin/prod" ]]; then
+        sh 'chmod +x build.sh'
+        sh './build.sh'
 
-        docker tag test ar8888/prod
-        docker push vennilavan/prod
+        docker tag test AarthiK/prod
+        docker push Aarthik/prod
 
 else
         echo "failed"
