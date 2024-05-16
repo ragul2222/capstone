a5a75df1e110c7227f1d509f3c2bca2b30de965b
AarthiK03 <155801991+AarthiK03@users.noreply.github.com>
2024-05-16 10:07:32 +0530

    created docker image

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+#npm install
+ #npm run build
 IMAGE_NAME="my-dockimage"
 TAG="latest"
-DOCKERFILE_PATH="d/onedrive/devops/capstone-project/docker/Dockerfile"  # Replace with the path to your Dockerfile
+#COPY build/ /usr/share/nginx/html
+DOCKERFILE_PATH="d/onedrive/devops/capstone-project/Docker"  # Replace with the path to your Dockerfile
 
 # Build the Docker image
 # docker build -t "$IMAGE_NAME:$TAG" -f "$DOCKERFILE_PATH" .
-docker buildx build --platform linux/amd64,linux/arm64 -t "$IMAGE_NAME:$TAG" -f "$DOCKERFILE_PATH" .
-
+#docker buildx build --platform linux/amd64,linux/arm64 -t "$IMAGE_NAME:$TAG" .
+docker build -t my-dockimage .
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
