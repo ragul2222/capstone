3866f9be99ae482697a1de1550560ca81cc39d1f
AarthiK03 <155801991+AarthiK03@users.noreply.github.com>
2024-05-16 11:37:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,13 @@ docker login -u aarthik  -p $DOCKER_PASSWORD
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
        sh 'chmod +x build.sh'
        sh './build.sh'
-
         docker tag test aarthik/dev
         docker push aarthik/dev
-
 if [[ $GIT_BRANCH == "origin/prod" ]]; then
         sh 'chmod +x build.sh'
         sh './build.sh'
-
         docker tag test aarthik/prod
         docker push aarthik/prod
-
 else
         echo "failed"
 fi
