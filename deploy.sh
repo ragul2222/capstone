feb29ee411a4a7f9c7756ca52f5e56791bf008ce
AarthiK03 <155801991+AarthiK03@users.noreply.github.com>
2024-05-16 10:53:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
-version: '3'
+#!/bin/bash
+docker-compose up -d
+docker login -u ar8888  -p dckr_pat_NqD6cUw-P2zyV9GQ715-Y8hLK_I
 
-services:
-  my-service:
-    image: my-dockimage:latest  
-    ports:
-      - "8080:80" 
-   
-    volumes:
-      - d/onedrive/devops/capstone-project/docker/Dockerfile:/var/opt/nginx
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+       sh'chmod +x build.sh'
+       sh'./build.sh'
+
+        docker tag test ar8888/dev
+        docker push vennilavan/dev
+
+if [[ $GIT_BRANCH == "origin/master" ]]; then
+        sh'chmod +x build.sh'
+        sh'./build.sh'
+
+        docker tag test ar8888/prod
+        docker push vennilavan/prod
+
+else
+        echo "failed"
+fi
