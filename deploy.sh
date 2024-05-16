5c76931eee5243fe832f453f78098beb93f4ba9c
AranganathanPrakash <114086134+AranganathanPrakash@users.noreply.github.com>
2024-05-16 11:56:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-docker login -u aarthik  -p $DOCKER_PASSWORD
+docker login -u ar8888  -p $DOCKER_PASSWORD
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
        sh 'chmod +x build.sh'
        sh './build.sh'
-        docker tag test aarthik/dev
-        docker push aarthik/dev
+        docker tag test ar8888/dev
+        docker push ar8888/dev
 if [[ $GIT_BRANCH == "origin/prod" ]]; then
         sh 'chmod +x build.sh'
         sh './build.sh'
-        docker tag test aarthik/prod
-        docker push aarthik/prod
+        docker tag test ar8888/prod
+        docker push ar8888/prod
 else
         echo "failed"
 fi
