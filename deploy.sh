0eb1c9f080c505ad6526da9a83a9f2b500c8e6c2
Ragul R R <127739974+ragul2222@users.noreply.github.com>
2025-02-05 10:08:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,10 @@
     docker login -u rrragul -p @Ragul@1403
     docker tag test rrragul/task_4
     docker push rrragul/task_4
+    # Stop and remove any existing container running on port 90
+    docker stop task_4_container || true
+    docker rm task_4_container || true
+
+    # Run the container on port 90
+    docker run -d -p 90:80 --name task_4_container rrragul/task_4
     
