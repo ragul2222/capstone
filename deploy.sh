f92a06ee18dbfdccb47bf520cc2895de16ee78ac
Ragul R R <127739974+ragul2222@users.noreply.github.com>
2025-02-05 09:33:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/ar
-    docker push ar8888/ar
+    docker login -u rrragul -p @Ragul@1403
+    docker tag test rrragul/sample
+    docker push rrragul/sample
     
